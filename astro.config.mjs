baaa85e26abd304ee5db79a3564626abe77c8142
Takayuki Moto <b1100770@planet.kanazawa-it.ac.jp>
2023-07-16 16:32:55 +0900

    fix/デプロイ後に画像が反映されないなどを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  server: { port: 3001 },
+  server: {
+    port: 3001
+  },
+  output: 'server',
+  adapter: vercel()
 });
